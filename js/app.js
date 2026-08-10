c07226eb1e649ea6cb8e784c43e1c20f58056cd9
SANJAY-O-U <sanjayou2005@gmail.com>
2026-08-10 22:00:48 +0530

    feat: build finance manager through brick 8

---
+++ js/app.js
@@ -0,0 +1,162 @@
+console.log("Finance Manager started!");
+
+const balanceElement = document.querySelector("#balance");
+const incomeElement = document.querySelector("#income");
+const expenseElement = document.querySelector("#expense");
+
+
+let transactions = [
+    {
+        id: 1,
+        title: "Salary",
+        amount: 50000,
+        category: "Job",
+        type: "income"
+    },
+
+    {
+        id: 2,
+        title: "Rent",
+        amount: 12000,
+        category: "Housing",
+        type: "expense"
+    },
+
+    {
+        id: 3,
+        title: "Food",
+        amount: 3000,
+        category: "Food",
+        type: "expense"
+    },
+    {
+        id:4,
+        title:"Freelance",
+        amount: 10000,
+        category:"Job",
+        type:"income"
+    },
+    {
+        id:5,
+        title:"Shopping",
+        amount: 2500,
+        category:"Shopping",
+        type:"expense"
+    }
+];
+let editingTransactionId= null;
+
+//Update Dashboard
+function updateDashboard(){
+const income = transactions
+    .filter(transaction => transaction.type === "income")
+    .reduce((total, transaction) => total + transaction.amount, 0);
+
+
+const expense = transactions
+    .filter(transaction => transaction.type === "expense")
+    .reduce((total, transaction) => total + transaction.amount, 0);
+
+
+const balance = income - expense;
+incomeElement.textContent = income;
+expenseElement.textContent = expense;
+balanceElement.textContent = balance;
+}
+updateDashboard();
+//render Transaction
+const transactionList=document.querySelector("#transactionList");
+function renderTransaction(transactionArray){
+    transactionList.innerHTML="";
+    for(let transaction of transactionArray){
+        const transactionElement=document.createElement("div");
+        transactionElement.classList.add("transaction");
+        transactionElement.innerHTML=`
+        <div id="${transaction.id}">
+        <br>
+            <strong>${transaction.title}</strong>
+            <span>₹${transaction.amount}</span>
+            <span>${transaction.category}</span>
+            <span>${transaction.type}</span>
+            <button class="delete-btn" data-id="${transaction.id}">Delete</button>
+            <button class="edit-btn" data-id="${transaction.id}">Edit</button>
+        <div>
+        `;
+        transactionList.appendChild(transactionElement);
+    }
+};
+//transaction From
+const transactionForm = document.querySelector("#transactionForm");
+const inputTitle=document.getElementById("title");
+const inputType=document.getElementById("type");
+const inputAmount=document.getElementById("amount");
+const inputCategory= document.getElementById("category");
+const submitBtn = document.querySelector("#submitBtn");
+transactionForm.addEventListener("submit",(e)=>{
+    console.log("Form Submitted");
+    e.preventDefault();
+    const transaction={
+        id:Date.now(),
+        title: inputTitle.value,
+        amount: Number(inputAmount.value),
+        category:inputCategory.value,
+        type: inputType.value
+    }
+    if(editingTransactionId===null){
+        transactions.push(transaction);
+    }
+    else{
+        const transaction_edit=transactions.find(element=>element.id===editingTransactionId);
+        transaction_edit.title=inputTitle.value;
+        transaction_edit.amount=Number(inputAmount.value);
+        transaction_edit.category=inputCategory.value;
+        transaction_edit.type=inputType.value;
+        editingTransactionId===null;
+    }
+    updateDashboard();
+    renderTransaction(transactions);
+    transactionForm.reset();
+    submitBtn.textContent = "Add Transaction";
+
+});
+//delete and edit Transaction
+transactionList.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("delete-btn")){
+        const transactionId= Number(e.target.dataset.id);
+        transactions=transactions.filter((transaction)=>transaction.id !=transactionId);
+        updateDashboard();
+        document.getElementById(transactionId).remove();
+    }
+    if (event.target.classList.contains("edit-btn")) {
+
+    const id = Number(event.target.dataset.id);
+
+    const transaction = transactions.find(
+        transaction => transaction.id === id
+    );
+    editingTransactionId= id;
+    inputTitle.value = transaction.title;
+    inputAmount.value = transaction.amount;
+    inputCategory.value = transaction.category;
+    inputType.value = transaction.type;
+    submitBtn.textContent = "Update Transaction";
+    }
+});
+const searchInput=document.querySelector("#searchInput");
+const typeFilter= document.querySelector("#typeFilter");
+function getFilteredTransactions(){
+    const searchTerm= searchInput.value.toLowerCase();
+    const selectedType= typeFilter.value;
+    let result= transactions;
+    result=result.filter(transaction=>transaction.title.toLowerCase().includes(searchTerm));
+    if(selectedType !== "all"){
+        result=result.filter(transaction=>transaction.type == selectedType);
+    }
+    return result;
+
+}
+
+searchInput.addEventListener("input",()=>{
+    console.log(searchInput);
+    renderTransaction(getFilteredTransactions());
+})
